387e73d923ee5f4a7c2f7542c34ab3466872e746
cweitkamp3 <cweitkamp3@gmail.com>
2023-03-21 17:20:06 +0100

    Initial commit.

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 
-# Compile the Boneel OT-Code
-
+# This package depends on the network simplex implementation by Nicolas Bonnel (link: https://github.com/nbonneel/network_simplex)
+# We have to complie the cooresponding library.
 const path_to_makefile = abspath(joinpath(@__DIR__, "../src/emd_bonneel"))
 
 try run(`which gcc`)
